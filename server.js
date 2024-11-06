e9bdb5e7b704179b301fa7c073e48b8127b5aaf5
Rasmus Kragh Wendelbo <>
2024-11-06 19:56:52 +0100

    remove SRP comment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js beskeder lægges i et array.
 // ekstra prompt er lagt i miljøvariabel.
-//specielt til SRP
+//
 const express = require('express');
 const dotenv = require('dotenv');
 const fetch = require('node-fetch');
